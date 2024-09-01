fb55ef5520c3af5693682a75e3658a10ad56ff51
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 00:38:27 +0300

    added color to the graph line

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x ** 2 for x in x_values]
 
 plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
 
 # Set graph and axes titles
 ax.set_title('Square Numbers', fontsize=24)
