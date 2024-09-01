2813c4661677fa5ca10c6d3ecc3f2f48ab9a4987
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 01:12:27 +0300

    added color map to highlight the steps order; additionally highlighted first and last points of the walk; disabled axes display to focus attention on the walk

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -11,11 +11,28 @@ while True:
     # Record random walk points on the graph
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(
+        rw.x_values,
+        rw.y_values,
+        c=point_numbers,
+        cmap=plt.cm.Blues,
+        edgecolors='none',
+        s=15
+    )
+
+    # Single out the first and last points
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(
+        rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100
+    )
+
+    # Hide the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
     keep_running = input('Generate a new walk? (y/n): ')
     if keep_running == 'n':
         break
-       
