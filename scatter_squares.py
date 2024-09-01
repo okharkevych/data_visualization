9a0cee8003a2d39faae6a550d5b2d10dc9e06815
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 00:37:23 +0300

    built a scatter graph for squares of 0-1000 numbers; number and square lists generated automatically

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values]
 
 plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set graph and axes titles
 ax.set_title('Square Numbers', fontsize=24)
@@ -14,5 +14,8 @@ ax.set_ylabel('Square of Value', fontsize=14)
 
 # Set font size for graph markings
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
