ed80bcb213b1611b247002e9465bb0e9bfc2b696
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 01:03:02 +0300

    added an option to generate multiple walks in a row based on user input

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -2,13 +2,20 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Generate random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Generate new walks while the program is active
+while True:
+    # Generate random walk
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Record random walk points on the graph
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    # Record random walk points on the graph
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
 
-plt.show()
+    plt.show()
+
+    keep_running = input('Generate a new walk? (y/n): ')
+    if keep_running == 'n':
+        break
+       
