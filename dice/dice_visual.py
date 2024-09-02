c50c357d565a908e2ad9971f0a612069a4a5aea4
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 15:31:42 +0300

    changed dies in dice_visual.py from 2 D6 to D6 and D10; increased roll quantity to 50000 times

--- dice/dice_visual.py
+++ dice/dice_visual.py
@@ -3,13 +3,13 @@ from plotly.graph_objs import Bar, Layout
 
 from die import Die
 
-# Create 2 D6 dies
+# Create D6 and D10 dies
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make several rolls and save the results to a list
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -27,8 +27,8 @@ data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(
-    title='Results of rolling two D6 dies 1000 times',
+    title='Results of rolling a D6 and a D10 dies 50000 times',
     xaxis=x_axis_config,
     yaxis=y_axis_config
 )
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
