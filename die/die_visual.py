1a18d575186bc3152aea379dba1f6708b66c98ca
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 15:15:37 +0300

    added roll results analysis in terms of frequencies of each side being the result; increased roll number to 1000; visualized results with a histogram; added '*.html' to .gitignore to ignore varying local visualization output files of plotly

--- die/die_visual.py
+++ die/die_visual.py
@@ -1,3 +1,6 @@
+from plotly import offline
+from plotly.graph_objs import Bar, Layout
+
 from die import Die
 
 # Create D6
@@ -5,8 +8,25 @@ die = Die()
 
 # Make several rolls and save the results to a list
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze results
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results
+x_values = list(range(1, die.num_sides))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(
+    title='Results of rolling one D6 1000 times',
+    xaxis=x_axis_config,
+    yaxis=y_axis_config
+)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
