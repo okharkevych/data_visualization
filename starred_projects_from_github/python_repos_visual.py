43f16dfaf0d009bc8f51621af1fe85cc911bb843
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-02 21:26:52 +0300

    added python_repos_visual.py; created visualization for most-starred Python projects on GitHub

---
+++ starred_projects_from_github/python_repos_visual.py
@@ -0,0 +1,34 @@
+import requests
+
+from plotly import offline
+
+# Make an API call and save the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'Status code: {r.status_code}')
+
+# Process results
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Create visualization
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+}]
+
+my_layout = {
+    'title': 'Most-Starred Python Projects on GitHub',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stars'},
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
